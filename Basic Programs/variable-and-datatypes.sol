dab8b7fac4d5a4560d5f8d7b0515765e22cfda04
m3blip <maitrimudumbi5113@gmail.com>
2025-02-27 14:38:35 +0530

    Create variable-and-datatypes.sol

---
+++ Basic Programs/variable-and-datatypes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract learnVariables{
+    //all our solidity code and all 3 data types
+    uint chocolateBar=10;
+    uint storeOwner=300;
+    bool lieDetector=true;
+    string errorMessageText="Error!There has been a mistake!";
+
+}
