579d42470089cab45442e2d90f4ad11ac0bb8ea5
m3blip <maitrimudumbi5113@gmail.com>
2025-03-04 12:51:43 +0530

    Update constructors.sol

--- intermediate pros/constructors.sol
+++ intermediate pros/constructors.sol
@@ -1,1 +1,23 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
+contract Member
+{
+    string name;
+    uint age;
+
+    constructor(string memory _name,uint _age)public{
+        name =_name;
+        age= _age;
+    }
+}
+
+contract Teacher is Member
+{
+    constructor(string memory n,uint a) Member(n,a)public{}
+
+    function getName() public view returns(string memory){
+        return name;
+    }
+    
+}
